c79ac7e873f378205bd60ffd3a43e8e04a359198
suhail <suhailajaz338@gmail.com>
2022-09-18 22:51:35 +0530

    Initial Commit

---
+++ MultiConverter/MultiConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiConverterApp.swift
+//  MultiConverter
+//
+//  Created by suhail on 18/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MultiConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
